57a35fe2d503ce9722b6a7a0c4bed38c88eb2ca3
Re2n <111135217+Re2n@users.noreply.github.com>
2023-09-30 16:01:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 board = list(range(1, 10))
+wins_coordinates = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
 
 
 def board_draw():
@@ -22,9 +23,21 @@ def get_input(player_token):
         break
 
 
+def check_win():
+    for each in wins_coordinates:
+        if (board[each[0] - 1]) == (board[each[1] - 1]) == (board[each[2] - 1]):
+            return board[each[1] - 1]
+    else:
+        return False
+
+
 def main():
     while True:
         board_draw()
+        winner = check_win()
+        if winner:
+            print(winner, ' выиграл')
+            break
         player_token = input('Выберите символ X или O: ')
         get_input(player_token)
 
